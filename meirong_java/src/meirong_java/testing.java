8b8a3561e3624bf5dded4df3145345fc714bb417
meiliu <meiliu@fusionlayer.com>
2018-01-20 22:46:17 +0200

    example

---
+++ meirong_java/src/meirong_java/testing.java
@@ -0,0 +1,14 @@
+package meirong_java;
+
+public abstract class testing {
+
+	public testing() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
